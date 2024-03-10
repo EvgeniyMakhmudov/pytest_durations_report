7a681ec9730ff718f1e21c2447868e445f68a44d
Evgeniy Makhmudov <john_16@list.ru>
2024-03-10 08:59:26 +0300

    Improove html report

--- report/html_report.go
+++ report/html_report.go
@@ -10,6 +10,8 @@ import (
 	"time"
 )
 
+const CELLWIDTH = 8
+
 func CreateHtmlReport(leaf *root.Leaf) {
 	dt := time.Now()
 	data := make(map[string]string)
@@ -22,7 +24,7 @@ func CreateHtmlReport(leaf *root.Leaf) {
 
 	// TODO: get title from first leaf title, based by cmd of launch pytest
 	data["title"] = fmt.Sprintf("Pytest durations report %s.html", dt.Format("2000-01-01 12:00:00"))
-	data["tree_body"] = strings.Join(buildLines(leaf), "\n")
+	data["tree_body"] = strings.Join(buildLines(leaf, len(leaf.Title)), "\n")
 
 	tmpl, error := template.New("template.html").ParseFiles("report/template.html")
 	if error != nil {
@@ -32,28 +34,73 @@ func CreateHtmlReport(leaf *root.Leaf) {
 	tmpl.Execute(file, data)
 }
 
-func buildLines(leaf *root.Leaf) []string {
-	result := make([]string, 0)
+func formatFloatToContentString(f float64) string {
+	fs := fmt.Sprintf("%.3f", f)
+	fs = strings.TrimSuffix(fs, ".000")
 
-	tmp := `
-	<span>%s</span>
-	<span>%.3f</span>
-	<span>%.3f</span>
-	<span>%.3f</span>
-	<span style="font-weight: bold">%.3f</span>
+	return fillNBSP(fs, CELLWIDTH, "left")
+}
+
+func fillNBSP(s string, width int, direction string) string {
+	if l := (width - len(s)); l > 0 {
+		if direction == "left" {
+			return strings.Repeat("&nbsp;", l) + s
+		} else {
+			return s + strings.Repeat("&nbsp;", l)
+		}
+	} else {
+		return s
+	}
+}
+
+func makeColorBar(a, b, c float64) string {
+	total := a + b + c
+	na := int((a * 100) / total)
+	nb := na + int((b*100)/total)
+	tmpl := "background-image: linear-gradient(to right, red 0%% %d%% , green %d%% %d%%, blue %d%% );"
+	//background-image: linear-gradient(to right, red 0% 25% , green 25% 75%, blue 75% );
+	return fmt.Sprintf(tmpl, na, na, nb, nb)
+}
+
+func makeContent(leaf *root.Leaf, max_title_length int) string {
+
+	tmp := `<span>%s</span>
+	<span style="background-color:#FFF5EE">%s</span>
+	<span style="background-color:#F0FFFF">%s</span>
+	<span style="background-color:#FFF5EE">%s</span>
+	<span style="background-color:#000;color:#FFF;font-weight: bold">%s</span>
+	<div style="display:inline-block;height:5px;width:150px;%s"></div>
 	`
-	content := fmt.Sprintf(tmp, leaf.Title, leaf.TimeSetup, leaf.TimeCall, leaf.TimeTearDown, leaf.TimeTotal)
+	content := fmt.Sprintf(
+		tmp,
+		fillNBSP(leaf.Title, max_title_length, "right"),
+		formatFloatToContentString(leaf.TimeSetup),
+		formatFloatToContentString(leaf.TimeCall),
+		formatFloatToContentString(leaf.TimeTearDown),
+		formatFloatToContentString(leaf.TimeTotal),
+		makeColorBar(leaf.TimeSetup, leaf.TimeCall, leaf.TimeTearDown),
+	)
+	return content
+}
+
+func buildLines(leaf *root.Leaf, max_title_length int) []string {
+	result := make([]string, 0)
+	content := makeContent(leaf, max_title_length)
 
 	if len(leaf.Childs) != 0 {
 		leafs := make([]*root.Leaf, 0, len(leaf.Childs))
+		max_title_length := 0
 		for _, v := range leaf.Childs {
 			leafs = append(leafs, v)
+			if len(v.Title) > max_title_length {
+				max_title_length = len(v.Title)
+			}
 		}
 		slices.SortFunc(leafs, root.LeafSortFunc)
 		result = append(result, "<li>", "<div class=\"drop dropM\">-</div>", content, "<ul>")
 
 		for _, leaf := range leafs {
-			lines := buildLines((leaf))
+			lines := buildLines(leaf, max_title_length)
 			result = append(result, lines...)
 		}
 
---
+++ report/html_report_test.go
@@ -0,0 +1,59 @@
+package report
+
+import "testing"
+
+func TestMakeColorBar(t *testing.T) {
+	cases := []struct {
+		a, b, c float64
+		want    string
+	}{
+		{1, 2, 1, "background-image: linear-gradient(to right, red 0% 25% , green 25% 75%, blue 75% );"},
+		{4, 5, 1, "background-image: linear-gradient(to right, red 0% 40% , green 40% 90%, blue 90% );"},
+	}
+
+	for _, c := range cases {
+		result := makeColorBar(c.a, c.b, c.c)
+		if result != c.want {
+			t.Errorf("Failed result %q, want %q", result, c.want)
+		}
+	}
+}
+
+func TestFormatFloatToContentString(t *testing.T) {
+	cases := []struct {
+		a    float64
+		want string
+	}{
+		{1, "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1"},
+		{123, "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;123"},
+		{1.2345, "&nbsp;&nbsp;&nbsp;1.234"},
+	}
+
+	for _, c := range cases {
+		result := formatFloatToContentString(c.a)
+		if result != c.want {
+			t.Errorf("Failed result %q, want %q", result, c.want)
+		}
+	}
+}
+
+func TestFillNBSP(t *testing.T) {
+	cases := []struct {
+		s         string
+		width     int
+		direction string
+		want      string
+	}{
+		{"qwe", 4, "left", "&nbsp;qwe"},
+		{"qwe", 4, "right", "qwe&nbsp;"},
+		{"qwe", 1, "left", "qwe"},
+		{"qwe", 1, "right", "qwe"},
+	}
+
+	for _, c := range cases {
+		result := fillNBSP(c.s, c.width, c.direction)
+		if result != c.want {
+			t.Errorf("Failed result %q, want %q", result, c.want)
+		}
+	}
+}

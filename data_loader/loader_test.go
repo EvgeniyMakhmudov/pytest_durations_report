feab14a1cd1ec0ba02876b10bf92efbd046d7b15
Evgeniy Makhmudov <john_16@list.ru>
2024-03-16 11:23:19 +0300

    Extend tests of data_loader package

--- data_loader/loader_test.go
+++ data_loader/loader_test.go
@@ -1,67 +1,100 @@
 package data_loader
 
 import (
+	"fmt"
 	"pytest_durations_report/nodes"
 	"reflect"
+	"strings"
 	"testing"
 )
 
-func TestParseStringToRecord1(t *testing.T) {
-	line := "2.84s setup    my_project/package_a/test_values.py::test_value1"
-	want := nodes.Record{
-		FullPath: "my_project/package_a/test_values.py/test_value1",
-		ItemName: "setup",
-		Value:    2.84,
+func TestParseStringToRecordSuccess(t *testing.T) {
+	cases := []struct {
+		line string
+		want nodes.Record
+	}{
+		{
+			"2.84s setup    my_project/package_a/test_values.py::test_value1",
+			nodes.Record{
+				FullPath: "my_project/package_a/test_values.py/test_value1",
+				ItemName: "setup",
+				Value:    2.84,
+			},
+		}, {
+			"1.5m call   my_project/package_a/test_values.py::test_value2[parametrize_values]",
+			nodes.Record{
+				FullPath: "my_project/package_a/test_values.py/test_value2/parametrize_values",
+				ItemName: "call",
+				Value:    90,
+			},
+		}, {
+			"1.5m call   my_project/package_a/test_values.py::test_value2[parametrize_values/some_value]",
+			nodes.Record{
+				FullPath: "my_project/package_a/test_values.py/test_value2/parametrize_values_some_value",
+				ItemName: "call",
+				Value:    90,
+			},
+		}, {
+			"1.5m call   my_project/package_a/test_values.py::test_value2[parametrize_values and some_value]",
+			nodes.Record{
+				FullPath: "my_project/package_a/test_values.py/test_value2/parametrize_values and some_value",
+				ItemName: "call",
+				Value:    90,
+			},
+		}, {
+			"2h call   my_project/package_a/test_values.py::test_value2[parametrize_values and some_value]",
+			nodes.Record{
+				FullPath: "my_project/package_a/test_values.py/test_value2/parametrize_values and some_value",
+				ItemName: "call",
+				Value:    7200,
+			},
+		}, {
+			"1d call   my_project/package_a/test_values.py::test_value2[parametrize_values and some_value]",
+			nodes.Record{
+				FullPath: "my_project/package_a/test_values.py/test_value2/parametrize_values and some_value",
+				ItemName: "call",
+				Value:    86400,
+			},
+		},
 	}
+	for _, c := range cases {
+		result := ParseStringToRecord(c.line)
 
-	result := ParseStringToRecord(line)
-
-	if !reflect.DeepEqual(result, want) {
-		t.Errorf("record %v not equal wanted %v", result, want)
+		if !reflect.DeepEqual(result, c.want) {
+			t.Errorf("record %v not equal wanted %v", result, c.want)
+		}
 	}
 }
 
-func TestParseStringToRecord2(t *testing.T) {
-	line := "1.5m call   my_project/package_a/test_values.py::test_value2[parametrize_values]"
-	want := nodes.Record{
-		FullPath: "my_project/package_a/test_values.py/test_value2/parametrize_values",
-		ItemName: "call",
-		Value:    90,
-	}
+// Runner function for run test which must panic, do partial check of error message
+func runner(line, prefix string, t *testing.T) {
+	defer func(prefix string) {
+		if r := recover(); r != nil {
+			error_s := fmt.Sprint(r)
+			if !strings.HasPrefix(error_s, prefix) {
+				t.Errorf("record must failed with error prefix, got %q, wanted %q", error_s, prefix)
+			}
+		} else {
+			t.Errorf("record must failed with error prefix, but was no panic")
 
-	result := ParseStringToRecord(line)
-
-	if !reflect.DeepEqual(result, want) {
-		t.Errorf("record %v not equal wanted %v", result, want)
-	}
+		}
+	}(prefix)
+	ParseStringToRecord(line)
 }
 
-func TestParseStringToRecord3(t *testing.T) {
-	line := "1.5m call   my_project/package_a/test_values.py::test_value2[parametrize_values/some_value]"
-	want := nodes.Record{
-		FullPath: "my_project/package_a/test_values.py/test_value2/parametrize_values_some_value",
-		ItemName: "call",
-		Value:    90,
+func TestParseStringToRecordFailed(t *testing.T) {
+	cases := []struct {
+		line, prefix string
+	}{
+		{
+			"2.84a setup    my_project/package_a/test_values.py::test_value1",
+			"unknown postfix",
+		}, {
+			"qm call   my_project/package_a/test_values.py::test_value2[parametrize_values]",
+			"error while convert duration string",
+		},
 	}
-
-	result := ParseStringToRecord(line)
-
-	if !reflect.DeepEqual(result, want) {
-		t.Errorf("record %v not equal wanted %v", result, want)
-	}
-}
-
-func TestParseStringToRecord4(t *testing.T) {
-	line := "1.5m call   my_project/package_a/test_values.py::test_value2[parametrize_values and some_value]"
-	want := nodes.Record{
-		FullPath: "my_project/package_a/test_values.py/test_value2/parametrize_values and some_value",
-		ItemName: "call",
-		Value:    90,
-	}
-
-	result := ParseStringToRecord(line)
-
-	if !reflect.DeepEqual(result, want) {
-		t.Errorf("record %v not equal wanted %v", result, want)
+	for _, c := range cases {
+		runner(c.line, c.prefix, t)
 	}
 }
